6b4a51d068f0549d46e5c44087b32ed028b08f8d
Josh Lehman <jalehman37@gmail.com>
2019-04-01 10:21:44 -0700

    update to our project

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
-(defproject shutdown "0.1.0-SNAPSHOT"
+(defproject starcity/shutdown "0.1.0-SNAPSHOT"
 
   :description "Manage a clojure application's shutdown hooks."
 
-  :url "https://github.com/zonotope/shutdown"
+  :url "https://github.com/starcity-properties/shutdown"
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :repositories [["releases" {:url        "s3://starjars/releases"
+                              :username   :env/aws_access_key
+                              :passphrase :env/aws_secret_key}]])
